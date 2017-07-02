dbd902e2ae66aa12b57eb4ee52abf7d1de66e7a7
gepeng820 <496636462@qq.com>
2017-07-02 16:13:20 +0800

    module.exports

--- 3.module/1.module.js
+++ 3.module/1.module.js
@@ -1,4 +1,5 @@
+console.log(con);
 //require exports module 都是这个文件外层套了一个闭包，闭包中的形参
 //exports,require,module,__dirname,__filename 也叫全局对象
 
-console.log(arguments);
+//console.log(arguments);
--- 第二天/4.global.js
+++ 第二天/4.global.js
@@ -25,3 +25,25 @@ setTimeout(function (data,a) {
     console.log(data,a);
     console.log(this);
 },1000,'你好','不好');//你好,setTimeout可以传递参数，这里的this指向setTimeout自己,文件中直接打印this 是空对象
+
+//3)setImmediate 立即 异步的 第二个小本上的
+setImmediate(function () {
+    console.log('立即');
+});//一般情况和不写时间的settimeout是一样的
+
+//4）process进程 设置环境变量， 生成环境 开发环境
+//在开发的时候输出开发环境 ，上线的时候输出生成环境
+console.log(process.env);//environment ，两个系统 mac windows
+//设置环境变量通过 set a=b 设置 可以通过process.env取出来(只在命令行下使用)，可以区分开发环境
+if(process.env.NODE_ENV==='dev'){
+    console.log('开发环境')
+}else {
+    console.log('生成环境')
+}
+
+//服务端全局变量 global  挂载在global上的属性 可以直接获取,var 声明的不会挂载在global上，为了解决模块化问题
+global.a=1;
+console.log(a);
+
+
+
